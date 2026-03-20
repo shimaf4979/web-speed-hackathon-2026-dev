e09be0da064244796476baf63650c9b2ed5ea4ec
shimayuu <cnt19024@ict.nitech.ac.jp>
2026-03-20 13:49:16 +0900

    Move Tailwind runtime to build-time CSS

--- application/client/postcss.config.js
+++ application/client/postcss.config.js
@@ -1,9 +1,11 @@
 const postcssImport = require("postcss-import");
+const tailwindcss = require("@tailwindcss/postcss");
 const postcssPresetEnv = require("postcss-preset-env");
 
 module.exports = {
   plugins: [
     postcssImport(),
+    tailwindcss(),
     postcssPresetEnv({
       stage: 3,
     }),
